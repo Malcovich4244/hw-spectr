d6a641088c7a511d633274c70c6120f1a61ed0a5
Илья Паньков <malcovich90@gmail.com>
2024-01-14 01:18:19 +0500

    task2|Point 4, filter is done

--- hw.py
+++ hw.py
@@ -1,6 +1,7 @@
-from functools import reduce
+
+years = [i for i in range(1950, 2025)] # генератор списка, годы с 1950 по 2024
 
 
-list_1 = ['Hello', 'World', 'Python', 'is', 'great!']
+# Лямбда функция проверяет, является ли год високосным
 
-print(reduce(lambda a, b: (a + ' ' + b).lower(), list_1))
+print(list(filter(lambda y: y % 4 == 0 or y % 400 == 0 and y % 100 != 0, years)))
