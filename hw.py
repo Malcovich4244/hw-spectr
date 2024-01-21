9387a402195e8b5750c280211b67025a45262d5b
Илья Паньков <malcovich90@gmail.com>
2024-01-22 00:55:48 +0500

    task3|Point 1 is done

--- hw.py
+++ hw.py
@@ -1,8 +1,34 @@
-sample_list = [1, 'hello', True, 42, 'world', False]
 
-'''Если x - число, то умножаем его на 2, иначе,
-если x - строка, то конвертируем каждый символ в верхний регистр, 
-иначе если x ложно, то возвращается True, иначе False'''
+def fibonachi_numbers(max_lenth: int) -> list:
+    # Генератор для чисел Фибоначчи до определенного предела
+    a, b = 0, 1
+    while b <= max_lenth:
+        # Пока текущее число не превысило предел, возвращаем его в качестве следующего числа
+        yield b
+        a, b = b, a + b
 
-print(list(map(lambda x: x * 2 if type(x) is int else x.upper() if type(x) is str
-               else not x, sample_list)))
+
+print(*fibonachi_numbers(100))
+
+
+class FibonachiNumbers:
+    def __init__(self, max_lenth: int) -> None:
+        self.a = 0
+        self.b = 1
+        self.max_lenth = max_lenth
+
+    def __iter__(self) -> None:
+        # Возвращаем сам объект в качестве итератора
+        return self
+
+    def __next__(self) -> int:
+        # Генерируем следующее число Фибоначчи до достижения предела
+        if self.b > self.max_lenth:
+            raise StopIteration
+        else:
+            result = self.b
+            self.a, self.b = self.b, self.a + self.b
+        return result
+
+
+print(*FibonachiNumbers(100))
